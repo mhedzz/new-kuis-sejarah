8fe87253c21c58d62d8ba7142ca8635aefceb593
Made Christian <christian.bali.919@gmail.com>
2024-11-11 22:06:50 +0800

    gatau update keberapa

---
+++ kuis/soalbab1.js
@@ -0,0 +1,117 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+        numb: 1,
+        question: "Salah satu pemicu pertempuran 10 November di Surabaya adalah",
+        answer: "Tewasnya Jendral Mallaby",
+        options: [
+          "Pembebasan tawanan perang tanpa persetujuan",
+          "Belanda mengingkari hasil perundingan Renville",
+          "Terjadinya kesalahpahaman",
+          "Tewasnya Jendral Mallaby"
+        ]
+      },
+      {
+        numb: 2,
+        question: "Tugas AFNEI di Indonesia, kecuali",
+        answer: "Memperbaiki keadaan ekonomi",
+        options: [
+          "Memperbaiki keadaan ekonomi",
+          "Menuntut penjahat perang",
+          "Membebaskan tahanan perang",
+          "Menegakkan dan mempertahankan keadaan Damai"
+        ]
+      },
+      {
+        numb: 3,
+        question: "Saat pertemuan komandan pasukan di Tebing Tinggi pada 10 Agustus 1946 dibentuk satu komando bernama",
+        answer: "Komando Resimen Laskar Rakyat Medan Area",
+        options: [
+          "Komando Tentara Keamanan Rakyat",
+          "Pertempuran Medan Area",
+          "Komando Resimen Laskar Rakyat Medan Area",
+          "Komando Laskar Pelangi"
+        ]
+      },
+      {
+        numb: 4,
+        question: "Mengapa disebut peristiwa Bandung Lautan Api",
+        answer: "Rakyat Bandung membakar bangunan-bangunan penting di Bandung bagian Selatan.",
+        options: [
+          "Rakyat Bandung menyerah kepada sekutu",
+          "Pihak Sekutu menguasai kota Bandung",
+          "Pihak Sekutu menguasai Bandung bagian utara",
+          "Rakyat Bandung membakar bangunan-bangunan penting di Bandung bagian Selatan."
+        ]
+      },
+      {
+        numb: 5,
+        question: "Bagaimana reaksi masyarakat ketika Sekutu mendarat di Semarang",
+        answer: "disambut baik",
+        options: [
+          "disambut baik",
+          "mengangkat senjata",
+          "masyarakat ketakutan",
+          "tidak peduli"
+        ]
+      },
+      {
+        numb: 6,
+        question: "Pertempuran Margarana terjadi di desa",
+        answer: "Marga",
+        options: [
+          "Penglipuran",
+          "Trunyan",
+          "Marga",
+          "Jatiluwih"
+        ]
+      },
+      {
+        numb: 7,
+        question: "Pertempuran Puputan Margarana dipimpin oleh",
+        answer: "I Gusti Ngurah Rai",
+        options: [
+          "Cut Nyak Dien",
+          "I Gusti Ngurah Rai",
+          "Pattimura",
+          "Jenderal Sudirman"
+        ]
+      },
+      {
+        numb: 8,
+        question: "Perang Puputan Margarana terjadi karena",
+        answer: "Belanda tidak mengakui Bali pada perjanjian Linggarjati",
+        options: [
+          "Belanda tidak mengakui Bali pada perjanjian Linggarjati",
+          "Terjadi Kesalahpahaman antara Belanda dengan rakyat Bali",
+          "Belanda ingin menguasai Bali",
+          "Adanya krisis ekonomi"
+        ]
+      },
+      {
+        numb: 9,
+        question: "Kepanjangan dari TKR adalah",
+        answer: "Tentara Keamanan Rakyat",
+        options: [
+          "Tentara Kesatuan Rakyat",
+          "Tentara Keadilan Rakyat",
+          "Tentara Keamanan Rakyat",
+          "Tentara Kepedulian Rakyat"
+        ]
+      }
+      
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
