0198d97b53906e8714ccdd3eda6bbe29ac351634
Made Christian <christian.bali.919@gmail.com>
2024-11-12 14:43:04 +0800

    new update

---
+++ kuis/soalbab2.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Program kabinet Baharudin Harahap yang benar adalah",
+    answer: "Mengembangkan rasa solidaritas antar anggota dan membendung pengaruh negatif dari blok Barat maupun blok Timur ke negara-negara anggota non-blok",
+    options: [
+      "Mengembangkan rasa solidaritas antar anggota dan membendung pengaruh positif dari blok Barat maupun blok Timur ke negara-negara non-blok",
+      "Ikut berperang bersama Amerika dan Uni Soviet",
+      "Mengembangkan rasa solidaritas antar anggota dan membendung pengaruh negatif dari blok Barat maupun blok Timur ke negara-negara anggota non-blok",
+      "Ikut berperang bersama Amerika dan Uni Soviet serta membendung pengaruh positif dari blok Barat maupun blok Timur"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Program kabinet Baharudin Harahap, kecuali",
+    answer: "Mewujudkan perekonomian yang makmur",
+    options: [
+      "Melaksanakan pemilihan umum",
+      "Pengembalian Irian Barat ke Republik Indonesia",
+      "Melaksanakan kerja sama Asia-Afrika berdasarkan politik bebas aktif",
+      "Mewujudkan perekonomian yang makmur"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Demokrasi Terpimpin dimulai sejak",
+    answer: "Dekrit Presiden 5 Juli 1959",
+    options: [
+      "Dwikora",
+      "Trikora",
+      "Dekrit Presiden 5 Juli 1959",
+      "17 Agustus 1945"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Dalam demokrasi liberal, Presiden berkedudukan sebagai",
+    answer: "kepala negara",
+    options: [
+      "kepala negara",
+      "kepala pemerintahan",
+      "panglima tertinggi",
+      "pimpinan eksekutif"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Pemberontakan DI/TII pertama kali terjadi di wilayah",
+    answer: "Jawa Barat",
+    options: [
+      "Kalimantan Selatan",
+      "D.I. Aceh",
+      "Jawa Barat",
+      "Sulawesi Selatan"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Kapan pembangunan Monas dilakukan",
+    answer: "17 Agustus 1961",
+    options: [
+      "12 Juli 1975",
+      "17 Agustus 1975",
+      "12 Juli 1961",
+      "17 Agustus 1961"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Apa itu konsep Nasakom pada masa demokrasi terpimpin",
+    answer: "Nasionalisme, Agama, Komunis",
+    options: [
+      "Nasional, Agamamis, Komunis",
+      "Nasionalisme, Agama, Komunis",
+      "Nasionalisme, Agama, Konsumerisme",
+      "Nasionalis, Agamamis, Komunisme"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Konferensi Asia Afrika pertama kali dilakukan di Kota",
+    answer: "Bandung",
+    options: [
+      "Kamboja",
+      "Bagdad",
+      "Manila",
+      "Bandung"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Partai politik yang menang pemilu 1955 adalah",
+    answer: "Nahdlatul Ulama",
+    options: [
+      "PDI Perjuangan",
+      "Nahdlatul Ulama",
+      "Gerindra",
+      "Perindo"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Salah satu latar belakang diselenggarakannya KAA adalah",
+    answer: "Menghapuskan segala bentuk penjajahan",
+    options: [
+      "Memajukan kerja sama antarbangsa Asia dan Afrika",
+      "Adanya pertentangan antara blok Barat dan blok Timur",
+      "Menghapuskan segala bentuk penjajahan",
+      "Meningkatkan kerja sama negara Asia Tenggara"
+    ]
+  }
+      
+];
---
+++ kuis/soalbab3.js
@@ -0,0 +1,114 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Apa nama tuntutan yang diajukan oleh kesatuan aksi Front Pancasila?",
+    answer: "Tritura",
+    options: [
+      "Tritura",
+      "Trikura",
+      "Dwikora",
+      "Dwikura"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Apa isi dari Tuntutan Hati Nurani Rakyat?",
+    answer: "Partai PKI dibubarkan, Membersihkan kabinet Dwikora dari unsur G30/S-PKI, Perbaiki ekonomi",
+    options: [
+      "Partai PKI dibubarkan, Membersihkan kabinet Dwikora dari unsur G30/S-PKI, Naikan harga ekonomi",
+      "Partai PKI dibubarkan, Membersihkan kabinet Dwikora dari unsur G30/S-PKI, Perbaiki ekonomi",
+      "Membersihkan kabinet Dwikora dari unsur G30/S-PKI, Naikan harga ekonomi, Perbaiki ekonomi",
+      "Partai PKI dibubarkan, Naikan harga ekonomi, Perbaiki ekonomi"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Surat perintah yang dikeluarkan oleh Soekarno bernama",
+    answer: "Supersemar 1966",
+    options: [
+      "Supersemar 1966",
+      "Supersemar 1977",
+      "Superstar 1966",
+      "Suparman 1977"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Kenapa bisa muncul dualisme kepemimpinan nasional?",
+    answer: "Soekarno sebagai pimpinan pemerintah tetapi yang menjalankan tugas adalah Soeharto",
+    options: [
+      "Adanya pembubaran PKI",
+      "Soekarno sebagai pimpinan pemerintah tetapi yang menjalankan tugas adalah Soeharto",
+      "Soekarno membubarkan kabinet Dwikora",
+      "Adanya tuntutan Hati Nurani Rakyat"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Apa penyebab Soekarno mengundurkan diri dari jabatannya sebagai Presiden?",
+    answer: "Sudah tidak mendapat dukungan politik lagi",
+    options: [
+      "Sistem pemerintahan Soekarno terlalu fokus pada Nasakom saja",
+      "PKI dibubarkan",
+      "Kabinet Dwikora dibubarkan",
+      "Sudah tidak mendapat dukungan politik lagi"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Bagi bangsa Indonesia, Supersemar memiliki arti yaitu, kecuali",
+    answer: "Letjen Soeharto mengambil tindakan untuk menjaga kestabilan pemerintahan Indonesia",
+    options: [
+      "Tonggak lahirnya Orde Baru",
+      "Awal penataan kehidupan sesuai Pancasila dan UUD 1945",
+      "Ketidakstabilan ekonomi",
+      "Letjen Soeharto mengambil tindakan untuk menjaga kestabilan pemerintahan Indonesia"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Letjen Soeharto dilantik menjadi pejabat presiden oleh",
+    answer: "Ketua MPRS",
+    options: [
+      "Perdana Menteri",
+      "TNI",
+      "Ketua MPRS",
+      "Ketua DPR"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Pemerintahan Soeharto dikenal sebagai",
+    answer: "Orde Baru",
+    options: [
+      "Demokrasi Terpimpin",
+      "Orde Baru",
+      "Orde Lama",
+      "Demokrasi Liberal"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Bagaimana reaksi masyarakat terhadap aksi demonstrasi 'Front Pancasila'?",
+    answer: "mendapatkan dukungan",
+    options: [
+      "tidak setuju",
+      "marah dan kecewa",
+      "tidak peduli",
+      "mendapatkan dukungan"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Apa unsur-unsur kabinet Dwikora?",
+    answer: "Pimpinan presiden Soekarno",
+    options: [
+      "Pimpinan presiden Soekarno",
+      "terdiri dari 10 menteri utama",
+      "Kabinet terdiri dari 25 menteri dibawah Presiden",
+      "Letjen Soeharto sebagai wakil ketua"
+    ]
+  }
+      
+];
